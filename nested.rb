bc3f1a67df5cf4c09df68fab2047f55ae93b1f0c
Ali Shah <ali1195shah@gmail.com>
2019-08-28 02:08:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -123,6 +122,4 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     return programmer_hash[:grace_hopper]
-
 end
